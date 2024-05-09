536a564ca5c51908fa0490a8d784c5ddb2d85be1
ao41O <105234618+ao41O@users.noreply.github.com>
2024-05-10 05:29:46 +0900

    Add files via upload

---
+++ ability.js
@@ -0,0 +1,99 @@
+export const abilityArray = {
+	"レギンレイヴ":{
+		multiplier:[
+			55
+		],
+		hit:[
+			5
+		],
+		cap:[
+			{start: 1, end: 1, max: 14999},
+		],
+		motionspeed: [0.86, 0],
+		cd: [15, 0],
+		charge: [0],
+		range: [1],
+		text:"前方に波動を放つ遠距離攻撃。<br>Class Lvに応じてヒット数が増加する"
+	},
+	"ディストリーム":{
+		multiplier:[
+			78
+		],
+		hit:[
+			11
+		],
+		cap:[
+			{start: 1, end: 1, max: 19999},
+		],
+		motionspeed: [1.66, 0],
+		cd: [30, 0],
+		charge: [0],
+		range: [0],
+		text:"連続ヒットする斬撃。<br>範囲が広く、前方の敵を巻き込みやすい。<br>Class Lvに応じてヒット数が増加する。"
+	},
+	"ドライブバースト":{
+		multiplier:[
+			627
+		],
+		hit:[
+			1
+		],
+		cap:[
+			{start: 1, end: 1, max: 89999},
+		],
+		motionspeed: [1.49, 0],
+		cd: [30, 0],
+		charge: [0],
+		range: [0],
+		text:""
+	},
+	"アーマーブレイク":{
+		multiplier:[
+			542
+		],
+		hit:[
+			1
+		],
+		cap:[
+			{start: 1, end: 1, max: 39999},
+		],
+		motionspeed: [0.89, 0],
+		cd: [45, 0],
+		charge: [0],
+		range: [0],
+		text:""
+	},
+	"アローレイン":{
+		multiplier:[
+			42
+		],
+		hit:[
+			4
+		],
+		cap:[
+			{start: 1, end: 1, max: 14999},
+		],
+		motionspeed: [1.51, 0],
+		cd: [45, 0],
+		charge: [0],
+		range: [0],
+		text:""
+	},
+	"ディスペル":{
+		multiplier:[
+			60
+		],
+		hit:[
+			1
+		],
+		cap:[
+			{start: 1, end: 1, max: 14999},
+		],
+		motionspeed: [1.12, 0],
+		cd: [60, 10],
+		charge: [0],
+		range: [1],
+		text:""
+	},
+}
+export default abilityArray
