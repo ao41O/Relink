13810bd3e709205822de859245f6eb8626026d56
ao41O <105234618+ao41O@users.noreply.github.com>
2024-05-10 05:37:54 +0900

    Add files via upload

--- data/ability.js
+++ data/ability.js
@@ -31,5 +31,69 @@ export const abilityArray = {
 		range: [0],
 		text:"連続ヒットする斬撃。<br>範囲が広く、前方の敵を巻き込みやすい。<br>Class Lvに応じてヒット数が増加する。"
 	},
+	"ドライブバースト":{
+		multiplier:[
+			627
+		],
+		hit:[
+			1
+		],
+		cap:[
+			{start: 1, end: 1, max: 89999},
+		],
+		motionspeed: [1.49, 0],
+		cd: [30, 0],
+		charge: [0],
+		range: [0],
+		text:""
+	},
+	"アーマーブレイク":{
+		multiplier:[
+			542
+		],
+		hit:[
+			1
+		],
+		cap:[
+			{start: 1, end: 1, max: 39999},
+		],
+		motionspeed: [0.89, 0],
+		cd: [45, 0],
+		charge: [0],
+		range: [0],
+		text:""
+	},
+	"アローレイン":{
+		multiplier:[
+			42
+		],
+		hit:[
+			4
+		],
+		cap:[
+			{start: 1, end: 1, max: 14999},
+		],
+		motionspeed: [1.51, 0],
+		cd: [45, 0],
+		charge: [0],
+		range: [0],
+		text:""
+	},
+	"ディスペル":{
+		multiplier:[
+			60
+		],
+		hit:[
+			1
+		],
+		cap:[
+			{start: 1, end: 1, max: 14999},
+		],
+		motionspeed: [1.12, 0],
+		cd: [60, 10],
+		charge: [0],
+		range: [1],
+		text:""
+	},
 }
 export default abilityArray
--- data/combo.js
+++ data/combo.js
@@ -16,6 +16,69 @@ export const combo = {
 			charge: [0],
 			range: [0]
 		},
+		"コンボB":{
+			multiplier:[
+				1, 30, 87, 22.5
+			],
+			hit:[
+				1, 1, 1, 2
+			],
+			cap:[
+				{start: 1, end: 2, max: 9999},
+				{start: 3, end: 4, max: 29999}
+			],
+			motionspeed: [3.26, 0],
+			finisher: [3, 4],
+			charge: [0],
+			range: [0]
+		},
+		"コンボC":{
+			multiplier:[
+				2, 30, 10, -38, -70, 28
+			],
+			hit:[
+				1, 1, 1, 1, 3, 1
+			],
+			cap:[
+				{start: 1, end: 4, max: 9999},
+				{start: 5, end: 6, max: 29999}
+			],
+			motionspeed: [4.23, 0],
+			finisher: [5, 6],
+			charge: [0],
+			range: [0]
+		},
+		"コンボD":{
+			multiplier:[
+				2, 30, -42, 10, -7, 47, 2, -22, 80
+			],
+			hit:[
+				1, 1, 1, 1, 1, 1, 1, 1, 1
+			],
+			cap:[
+				{start: 1, end: 6, max: 9999},
+				{start: 7, end: 9, max: 29999}
+			],
+			motionspeed: [4.71, 0],
+			finisher: [7, 8, 9],
+			charge: [0],
+			range: [0]
+		},
+		"パワーレイズ":{
+			multiplier:[
+				31, 255
+			],
+			hit:[
+				4, 1
+			],
+			cap:[
+				{start: 1, end: 2, max: 29999}
+			],
+			motionspeed: [3.06, 2],
+			finisher: [0],
+			charge: [1, 2],
+			range: [0]
+		},
 		"コンボD＋パワーレイズ":{
 			multiplier:[
 				2, 30, -42, 10, -7, 47, 2, -22, 80, 31, 255
